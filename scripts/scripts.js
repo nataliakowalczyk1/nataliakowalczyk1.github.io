101c800a3154dbf1133c6d62abbada238b1ed0cf
Natalia Kowalczyk <nkowalczyk@vp.pl>
2022-11-20 18:38:22 +0100

    przekazuje pliki <3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,3 +6,15 @@ function pokażZwierzęta() {
         }
     )
 }
+
+function rysujKoty() {
+    var ileKotow = 79;
+    for (var i=0; i <ileKotow; i++) {
+        var kotek = document.createElement('p');
+        kotek.classList.add("czerwony-kotek");
+        var twarzKotka = document.createTextNode(i + "=^.^=");
+        kotek.appendChild(twarzKotka);
+        var piesek = document.getElementById("piesek");
+        document.body.insertBefore(kotek, piesek);
+    }
+}; 
